66d7604a2df240b8fdee6d657031e397766e77a7
Filipe <filipe.rj149@gmail.com>
2023-06-30 03:48:00 +0000

    adiciona scripts `lint:check` e `lint:fix`

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,14 @@
 function Home() {
-    return <h1>Te amo pequena gênia. Meu amor por você não é apenas como uma faísca de memória e não sofre de síndrome de Dory. É intenso, uma chama quente como eu fico quando te abraço. Você me vem à mente a todo instante e desde que colocamos nossos 💍 posso abrir um sorriso descansado ao lembrar que te tenho. Obrigado por fazer meus dias mais interessantes ao compartilhar a vida comigo 💖</h1>;
+  return (
+    <h1>
+      Te amo pequena gênia. Meu amor por você não é apenas como uma faísca de
+      memória e não sofre de síndrome de Dory. É intenso, uma chama quente como
+      eu fico quando te abraço. Você me vem à mente a todo instante e desde que
+      colocamos nossos 💍 posso abrir um sorriso descansado ao lembrar que te
+      tenho. Obrigado por fazer meus dias mais interessantes ao compartilhar a
+      vida comigo 💖
+    </h1>
+  );
 }
 
 export default Home;
