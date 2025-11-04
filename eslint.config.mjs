6cced10bfe600ed92f1b08bfafae73804e47c365
Filipe <filipe.rj149@gmail.com>
2025-11-04 00:06:30 +0000

    fix `eslint` problems

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -3,14 +3,26 @@ import globals from "globals";
 import pluginReact from "eslint-plugin-react";
 import css from "@eslint/css";
 import { defineConfig, globalIgnores } from "eslint/config";
+import pluginJest from "eslint-plugin-jest";
 
 export default defineConfig([
   globalIgnores([".next"]),
   {
+    settings: {
+      react: {
+        version: "detect",
+      },
+    },
     files: ["**/*.{js,mjs,cjs,jsx}"],
     plugins: { js },
     extends: ["js/recommended"],
-    languageOptions: { globals: { ...globals.browser, ...globals.node } },
+    languageOptions: {
+      globals: {
+        ...globals.browser,
+        ...globals.node,
+        ...pluginJest.environments.globals.globals,
+      },
+    },
   },
   pluginReact.configs.flat.recommended,
   {
